992f66c507e92bfea628268a71060689c5168811
lê <112022669+lecristinaa@users.noreply.github.com>
2024-08-06 11:57:37 -0300

    Add files via upload
    Projeto sobre dados do tabuleiro

---
+++ src/screens/MainScreen.jsx
@@ -0,0 +1,22 @@
+import { SafeAreaView, View, Button } from "react-native"
+
+export default function MainScreen() {
+    return(
+        <View>
+            <View>
+                <Text></Text>
+                <Text></Text>
+            </View>
+
+            <View>
+                <Button> D4 </Button>
+                <Button> D6 </Button>
+                <Button> D8 </Button>
+                <Button> D10 </Button>
+                <Button> D12 </Button>
+                <Button> D20 </Button>
+                <Button> D100 </Button>
+            </View>
+        </View>
+    )
+}
